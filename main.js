159884539c0f41dc0d2aef127ac33001dfba6f77
michaelb2001 <brunco.michael@gmail.com>
2022-01-11 13:58:12 +0100

    like

--- main.js
+++ main.js
@@ -56,47 +56,70 @@ const posts = [
     }
 ];
 
-let container = document.getElementById("container");
+
+function creaPost(array){
+    let container = document.getElementById("container");
+    for (let i = 0; i < array.length; i++) {
+        container.innerHTML += 
+        `
+        <div class="post" id="post-${i+1}">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src=${array[i].author.image} alt="${array[i].author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${array[i].author.name}</div>
+                        <div class="post-meta__time">${array[i].created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${array[i].content}</div>
+            <div class="post__image">
+                <img src=${array[i].media} alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="1">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-${i}" class="js-likes-counter">${array[i].likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+
+
+        `; 
+    }
+
+
+};
 
 creaPost(posts);
 
-function creaPost(array){
-for (let i = 0; i < array.length; i++) {
-    container.innerHTML += 
-    `
-    <div class="post">
-        <div class="post__header">
-            <div class="post-meta">                    
-                <div class="post-meta__icon">
-                    <img class="profile-pic" src=${array[i].author.image} alt="${array[i].author.name}">                    
-                </div>
-                <div class="post-meta__data">
-                    <div class="post-meta__author">${array[i].author.name}</div>
-                    <div class="post-meta__time">${array[i].created}</div>
-                </div>                    
-            </div>
-        </div>
-        <div class="post__text">${array[i].content}</div>
-        <div class="post__image">
-            <img src=${array[i].media} alt="">
-        </div>
-        <div class="post__footer">
-            <div class="likes js-likes">
-                <div class="likes__cta">
-                    <a class="like-button  js-like-button" href="#" data-postid="1">
-                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
-                        <span class="like-button__label">Mi Piace</span>
-                    </a>
-                </div>
-                <div class="likes__counter">
-                    Piace a <b id="like-counter-1" class="js-likes-counter">${array[i].likes}</b> persone
-                </div>
-            </div> 
-        </div>            
-    </div>
+let likeButtons = document.querySelectorAll(".js-like-button");
+let likeButtons_liked = document.querySelectorAll(".like-button--liked");
 
+for (let i = 0; i < likeButtons.length; i++) {
+    likeButtons[i].addEventListener("click",function(){
 
-    `; 
-}
+        this.classList.add("like-button--liked");
+
+        document.getElementById("like-counter-"+i).innerHTML = posts[i].likes+1;
+        /*for (let i = 0; i < likeButtons_liked.length; i++) {
+    
+            likeButtons_liked.addEventListener("click",function(){
+        
+                likeButtons.classList.remove("like-button--liked");
+        
+            });
+        }*/
+    });
+
 
 }
